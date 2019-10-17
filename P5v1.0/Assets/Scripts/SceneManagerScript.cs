5903d8b85117da80b16b120c45f01fcd8c215e7d
Thomas <tbork17@student.aau.dk>
2019-10-17 11:54:13 +0200

    SceneManager
    changes scene after an counter

---
+++ P5v1.0/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public float counter;
+    public int ChangeSceneAtSec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = Time.deltaTime;
+
+        //SceneManager.LoadScene("Sceneworkspace");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // counter in secs
+        counter += Time.deltaTime;
+        // When to change scene
+        if (counter > ChangeSceneAtSec) {
+            int scene = SceneManager.GetActiveScene().buildIndex;
+            // Change to next scene
+            SceneManager.LoadScene(scene + 1);
+        }
+    }
+}
